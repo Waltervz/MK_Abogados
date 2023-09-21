bde9fb1b24e00c22f45b4d0e58b183239fc05149
Walter <waltervz278@gmail.com>
2023-09-21 08:56:31 -0300

    ultima actualizacion

--- js/script.js
+++ js/script.js
@@ -18,4 +18,41 @@ window.addEventListener('scroll', () => {
     logo.classList.add('visible');
   }
 });
-  
+
+// Selecciona todos los elementos con la clase "carousel"
+const carousels = document.querySelectorAll('.carousel');
+
+// Agrega un evento de clic a cada elemento "carousel"
+carousels.forEach(carousel => {
+  const items = carousel.querySelectorAll('.carousel-item');
+  let index = 0;
+
+  // Función para mostrar el siguiente elemento del carrusel
+  const nextSlide = () => {
+    items[index].classList.remove('active');
+    index = (index + 1) % items.length;
+    items[index].classList.add('active');
+  };
+
+  // Función para mostrar el elemento anterior del carrusel
+  const prevSlide = () => {
+    items[index].classList.remove('active');
+    index = (index - 1 + items.length) % items.length;
+    items[index].classList.add('active');
+  };
+
+  // Agrega eventos de clic a los botones de control (opcional)
+  const nextButton = carousel.querySelector('.next-button');
+  if (nextButton) {
+    nextButton.addEventListener('click', nextSlide);
+  }
+
+  const prevButton = carousel.querySelector('.prev-button');
+  if (prevButton) {
+    prevButton.addEventListener('click', prevSlide);
+  }
+
+  // Inicializa el carrusel
+  items[index].classList.add('active');
+});
+
